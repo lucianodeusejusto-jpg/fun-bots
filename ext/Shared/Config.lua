05bab021171fd9a6e705cd00893145bdcba4d07b
lucianodeusejusto-jpg <lucianodeusejusto@gmail.com>
2026-09-12 03:03:28 -0300

    Config.lua

--- ext/Shared/Config.lua
+++ ext/Shared/Config.lua
@@ -11,19 +11,19 @@ Config = {
 	BotTeamNames = false,				-- If the bots shall have their names based on their team 
 
 	-- DIFFICULTY 
-	BotAimWorsening = 0.2,				-- Make bots aim worse: for difficulty: 0 = no offset (hard), 1 or even greater = more sway (easy) 
-	BotSniperAimWorsening = 0.1,		-- See botAimWorsening, only for Sniper-rifles 
-	BotSupportAimWorsening = 0.3,		-- See botAimWorsening, only for LMGs 
-	BotWorseningSkill = 0.40,			-- Variation of the skill of a single bot. The higher, the worse the bots can get compared to the original settings 
-	BotSniperWorseningSkill = 0.30,		-- See BotWorseningSkill - only for BOTs using sniper bolt-action rifles 
+	BotAimWorsening = 7.0,				-- Make bots aim worse: for difficulty: 0 = no offset (hard), 1 or even greater = more sway (easy) 
+	BotSniperAimWorsening = 7.1,		-- See botAimWorsening, only for Sniper-rifles 
+	BotSupportAimWorsening = 7.3,		-- See botAimWorsening, only for LMGs 
+	BotWorseningSkill = 7.40,			-- Variation of the skill of a single bot. The higher, the worse the bots can get compared to the original settings 
+	BotSniperWorseningSkill = 7.30,		-- See BotWorseningSkill - only for BOTs using sniper bolt-action rifles 
 	ReactionTime = 0.2,					-- Additional delay for bots, dependant of skill (might also be 0) 
-	DamageFactorAssault = 0.8,			-- Original Damage from bots gets multiplied by this 
-	DamageFactorCarabine = 0.8,			-- Original Damage from bots gets multiplied by this 
-	DamageFactorLMG = 0.8,				-- Original Damage from bots gets multiplied by this 
-	DamageFactorPDW = 0.8,				-- Original Damage from bots gets multiplied by this 
-	DamageFactorSniper = 0.8,			-- Original Damage from bots gets multiplied by this 
-	DamageFactorShotgun = 0.8,			-- Original Damage from bots gets multiplied by this 
-	DamageFactorPistol = 0.8,			-- Original Damage from bots gets multiplied by this 
+	DamageFactorAssault = 0.77,			-- Original Damage from bots gets multiplied by this 
+	DamageFactorCarabine = 0.77,			-- Original Damage from bots gets multiplied by this 
+	DamageFactorLMG = 0.77,				-- Original Damage from bots gets multiplied by this 
+	DamageFactorPDW = 0.77,				-- Original Damage from bots gets multiplied by this 
+	DamageFactorSniper = 0.77,			-- Original Damage from bots gets multiplied by this 
+	DamageFactorShotgun = 0.77,			-- Original Damage from bots gets multiplied by this 
+	DamageFactorPistol = 0.77,			-- Original Damage from bots gets multiplied by this 
 	DamageFactorKnife = 1.5,			-- Original Damage from bots gets multiplied by this 
 	VehicleAimWorsening = 0.05,			-- Make bots in vehicles aim worse: for difficulty: 0 = no offset (hard), 1 or even greater = more sway (easy) 
 	VehicleAAAimWorsening = 0.9,		-- Make bots in AA aim worse: for difficulty: 0 = no offset (hard), 1 or even greater = more sway (easy) 
